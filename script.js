be4a6a232ce1315306d549f3a9181c3f35e8b6ab
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-05-31 20:17:21 +0530

    delegation

--- script.js
+++ script.js
@@ -119,3 +119,58 @@ allSections.forEach(function (section) {
 
   section.classList.add("section--hidden");
 });
+
+// lazy loading , images
+
+// selecting the images
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry.target);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.classList.remove("lazy-img");
+
+  entry.target.addEventListener("load", function () {});
+};
+
+//img observer
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+// const imgTargets = document.querySelectorAll("img[data-src]");
+
+// const loadImg = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+
+//   // Replace src with data-src
+//   entry.target.src = entry.target.dataset.src;
+
+//   entry.target.addEventListener("load", function () {
+//     entry.target.classList.remove("lazy-img");
+//   });
+
+//   observer.unobserve(entry.target);
+// };
+
+// const imgObserver = new IntersectionObserver(loadImg, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: "200px",
+// });
+
+// imgTargets.forEach((img) => imgObserver.observe(img));
